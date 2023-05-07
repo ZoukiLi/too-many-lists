153c3163528a9a354ca983061b160905e641ec63
zoukiri debian <zoukiri180@gmail.com>
2023-05-07 20:10:35 +0800

    Add: Third list.

---
+++ lists/src/third.rs
@@ -0,0 +1,123 @@
+use std::sync::Arc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Arc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> Self {
+        List {
+            head: Some(Arc::new(Node {
+                elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn tail(&self) -> Self {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    // We want to return a reference to the data inside the node, so we use
+    // `Option<&T>`.
+    type Item = &'a T;
+
+    // The `next` method returns the next value in the iterator.
+    fn next(&mut self) -> Option<Self::Item> {
+        // Grab the next node from the iterator.
+        self.next.map(|node| {
+            // Update our state to point to the next node.
+            self.next = node.next.as_deref();
+
+            // Return a reference to the data in the node we just took.
+            &node.elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        // We need to take each node's `next` field and replace it with `None`
+        // before the node gets dropped. This is the only way to get the
+        // reference counts to work out correctly.
+        let mut head = self.head.take();
+
+        // We need to use a `while let` loop here because we need to take
+        // ownership of the `next` field in the `Some` variant of `head`.
+        while let Some(node) = head {
+            if let Ok(mut node) = Arc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+}
